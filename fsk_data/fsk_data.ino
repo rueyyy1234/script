c0f1c78ab440dbb28f496b32086f23436d7952ac
ruey@mac <rueyseong2001@gmail.com>
2025-12-02 14:56:43 +0900

    update

--- fsk_data/fsk_data.ino
+++ fsk_data/fsk_data.ino
@@ -4,19 +4,41 @@
 #define MOD_PIN 6
 #define BUTTON_PIN 5
 
-#define SWITCHING (1 << 0)
-#define LOW_FREQ_ONLY (1 << 1)
-#define HIGH_FREQ_ONLY (1 << 2)
+#define PORT SerialUSB
 
 #define READING (1 << 0)
 #define WRITING (1 << 1)
+#define INITIATING (1 << 2)
+#define NO_SERIAL (1 << 3)
 
-uint8_t switching_mode = SWITCHING;
-uint8_t state = READING;
+#define LOW_FREQ_ONLY (1 << 0)
+#define HIGH_FREQ_ONLY (1 << 1)
+#define SWITCHING (1 << 2)
 
+
+uint8_t state;
+uint8_t switching_mode;
 volatile uint8_t current_byte = 0;
 volatile uint8_t bit_idx = 0;
-volatile uint16_t bit_interval_us = 20;  // 100us per bit
+uint16_t bit_interval_us;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MOD_PIN, OUTPUT);
+  pinMode(CONTROL_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    switching_mode = LOW_FREQ_ONLY;
+    state = NO_SERIAL;
+    attachInterrupt(BUTTON_PIN, button_isr_handler, FALLING);
+  } else {
+    switching_mode = SWITCHING;
+    state = INITIATING;
+    PORT.begin(115200);
+    Timer1.attachInterrupt(timer_handler);
+  }
+}
 
 void button_isr_handler() {
   if (switching_mode == LOW_FREQ_ONLY) {
@@ -31,73 +53,61 @@ void timer_handler() {
 
   uint8_t bit = (current_byte >> (7 - bit_idx)) & 0x01;
 
-  if (bit) {
-    digitalWrite(MOD_PIN, HIGH);
-    digitalWrite(CONTROL_PIN, LOW);
-  } else {
-    digitalWrite(MOD_PIN, LOW);
-    digitalWrite(CONTROL_PIN, HIGH);
-  }
+  digitalWrite(MOD_PIN, bit ? HIGH : LOW);
+  digitalWrite(CONTROL_PIN, bit ? LOW : HIGH);
 
   bit_idx++;
 
   if (bit_idx >= 8) {
     bit_idx = 0;
-    state = READING;  // Back to reading next byte
+    state = READING;
   }
 }
 
-void setup() {
-  pinMode(BUTTON_PIN, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(MOD_PIN, OUTPUT);
-  pinMode(CONTROL_PIN, OUTPUT);
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-  // If the button is HIGH at boot enter  no switching mode
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-    switching_mode = LOW_FREQ_ONLY;
-    attachInterrupt(BUTTON_PIN, button_isr_handler, FALLING);
-    // Else enter switching mode
-  } else {
-    Serial.begin(115200);
-    switching_mode = SWITCHING;
-    Timer1.attachInterrupt(timer_handler);
-    Timer1.setPeriod(bit_interval_us);
-    Timer1.start();
-  }
-}
-
+uint8_t idx = 0;
 void loop() {
-
-  while (switching_mode == LOW_FREQ_ONLY || switching_mode == HIGH_FREQ_ONLY) {
+  idx++;
+  Serial.println(data[idx]);
+  while (state == NO_SERIAL) {
     switch (switching_mode) {
       case LOW_FREQ_ONLY:
         digitalWrite(MOD_PIN, LOW);
         digitalWrite(CONTROL_PIN, HIGH);
 
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
+        delay(600);
         digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
+        delay(600);
         break;
-
       case HIGH_FREQ_ONLY:
         digitalWrite(MOD_PIN, HIGH);
         digitalWrite(CONTROL_PIN, LOW);
 
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(600);
+        delay(100);
         digitalWrite(LED_BUILTIN, LOW);
-        delay(600);
+        delay(100);
+        break;
+      default:
+        digitalWrite(MOD_PIN, LOW);
+        digitalWrite(CONTROL_PIN, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
         break;
     }
   }
 
-  while (Serial.available() && state == READING) {
-    current_byte = Serial.read();
-    state = WRITING;
+  if (state == INITIATING && PORT.available() >= 2) {
+    PORT.readBytes((char*)&bit_interval_us, 2);
+    Timer1.setPeriod(bit_interval_us);
+    Timer1.start();
+
+    state = READING;
+  }
+
+  while (state == READING && PORT.available()) {
+    current_byte = PORT.read();
     bit_idx = 0;
+    state = WRITING;
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
--- fsk_data/send_fsk_data.m
+++ fsk_data/send_fsk_data.m
@@ -1,12 +1,41 @@
-clear;
+clear; clc
 delete(serialportfind);
 
-devName = '/dev/tty.usbmodem1301';
-dev = serialport(devName,460800);
-flush(dev);
+devName = "/dev/cu.usbmodem11201";
+dev = serialport(devName,115200);
 
-data = repelem(170,20);
+rand_data = readmatrix("test.csv");
+configureCallback(dev, "terminator", @printRead);
 
-while (true)
-    write(dev, uint8(data), "uint8");
+bit_interval_us = uint16(5000);
+write(dev, bit_interval_us, "uint16");
+
+% wait for Arduino to confirm
+pause(0.5);
+runtime_us = double(length(rand_data))* 8 * double(bit_interval_us);
+printRuntime(runtime_us);
+
+for k = 1:length(rand_data)
+    write(dev, uint8(rand_data(k)), "uint8");
+end
+
+function printRead(src, ~)
+    data = read(src, src.NumBytesAvailable, "uint8");
+    disp(char(data)')
+end
+
+function printRuntime(us)
+if us >= 1e6
+    value = us / 1e6;
+    unit = 's';
+elseif us >= 1e3
+    value = us / 1e3;
+    unit = 'ms';
+else
+    value = us;
+    unit = 'us';
+end
+
+fprintf('Estimated runtime: %.3f %s\n', value, unit);
+
 end
