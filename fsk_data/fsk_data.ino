cbbce9a71d7cd3871dd3f82f127137fd88fb32d0
ruey@mac <rueyseong2001@gmail.com>
2025-12-03 17:45:41 +0900

    update

--- fsk_data/fsk_data.ino
+++ fsk_data/fsk_data.ino
@@ -1,25 +1,42 @@
 #include <DueTimer.h>
 
+#define INCLUDE_DATA
 #define CONTROL_PIN 7
 #define MOD_PIN 6
 #define BUTTON_PIN 5
 
 #define PORT SerialUSB
 
-#define READING (1 << 0)
-#define WRITING (1 << 1)
-#define INITIATING (1 << 2)
-#define NO_SERIAL (1 << 3)
+
+#if defined(INCLUDE_DATA)
+#define INIT (1 << 0)
+#define START (1 << 1)
+#define END (1 << 2)
+#endif
+#define READING (1 << 3)
+#define WRITING (1 << 4)
+#define INITIATING (1 << 5)
+#define NO_SERIAL (1 << 6)
+
 
 #define LOW_FREQ_ONLY (1 << 0)
 #define HIGH_FREQ_ONLY (1 << 1)
 #define SWITCHING (1 << 2)
 
+void timer_handler();
+void button_isr_handler();
 
-uint8_t state;
 uint8_t switching_mode;
 volatile uint8_t current_byte = 0;
+#if defined(INCLUDE_DATA)
+#include "data.h"
+uint8_t state = INIT;
+uint32_t bit_idx = 0;
+#else
+uint8_t state;
 volatile uint8_t bit_idx = 0;
+#endif
+
 uint16_t bit_interval_us;
 
 void setup() {
@@ -33,10 +50,18 @@ void setup() {
     state = NO_SERIAL;
     attachInterrupt(BUTTON_PIN, button_isr_handler, FALLING);
   } else {
+#if defined(INCLUDE_DATA)
+    Timer1.attachInterrupt(timer_handler);
+    Timer1.setPeriod(BIT_INTERVAL_US);
+    Timer1.start();
+#else
     switching_mode = SWITCHING;
     state = INITIATING;
     PORT.begin(115200);
     Timer1.attachInterrupt(timer_handler);
+    digitalWrite(MOD_PIN, LOW);
+    digitalWrite(CONTROL_PIN, HIGH);
+#endif
   }
 }
 
@@ -49,19 +74,26 @@ void button_isr_handler() {
 }
 
 void timer_handler() {
+#if defined(INCLUDE_DATA)
+  uint8_t bit = (uint8_t)(rand_data[bit_idx] & 0x01);
+  digitalWrite(MOD_PIN, bit ? HIGH : LOW);
+  digitalWrite(CONTROL_PIN, bit ? LOW : HIGH);
+  bit_idx++;
+  if (bit_idx >= (uint32_t)BIT_NO) {
+    state = END;
+  }
+#else
   if (state == READING) return;
-
   uint8_t bit = (current_byte >> (7 - bit_idx)) & 0x01;
 
   digitalWrite(MOD_PIN, bit ? HIGH : LOW);
   digitalWrite(CONTROL_PIN, bit ? LOW : HIGH);
-
   bit_idx++;
-
   if (bit_idx >= 8) {
     bit_idx = 0;
     state = READING;
   }
+#endif
 }
 
 void loop() {
@@ -93,10 +125,20 @@ void loop() {
     }
   }
 
+#if defined(INCLUDE_DATA)
+  if (state == INIT) {
+    state = START;
+    digitalWrite(LED_BUILTIN, HIGH);
+    while (state != END) {
+      ;
+    }
+    Timer1.stop();
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+#else
   if (state == INITIATING && PORT.available() >= 2) {
     PORT.readBytes((char*)&bit_interval_us, 2);
     Timer1.setPeriod(bit_interval_us);
-    Timer1.start();
 
     state = READING;
   }
@@ -107,4 +149,5 @@ void loop() {
     state = WRITING;
     digitalWrite(LED_BUILTIN, HIGH);
   }
+#endif
 }
